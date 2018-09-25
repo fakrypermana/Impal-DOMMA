cad46ecd33697f19081d683e78256839de0bbdd5
deanabn <deandraabiantoro@gmail.com>
2018-09-25 23:46:53 +0700

    tambah model kategori

--- app/src/main/java/com/id/start/domma_v2/model/kategori.java
+++ app/src/main/java/com/id/start/domma_v2/model/kategori.java
@@ -1,4 +1,27 @@
 package com.id.start.domma_v2.model;
 
 public class kategori {
+    String namaKategori;
+    int tipeKategori;
+
+    public kategori(String namaKategori, int tipeKategori) {
+        this.namaKategori = namaKategori;
+        this.tipeKategori = tipeKategori;
+    }
+
+    public String getNamaKategori() {
+        return namaKategori;
+    }
+
+    public void setNamaKategori(String namaKategori) {
+        this.namaKategori = namaKategori;
+    }
+
+    public int getTipeKategori() {
+        return tipeKategori;
+    }
+
+    public void setTipeKategori(int tipeKategori) {
+        this.tipeKategori = tipeKategori;
+    }
 }
