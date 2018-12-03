a5c728f6694b98dd31bafe6726174907c34892b3
raihaniyai <raihaniyai@gmail.com>
2018-12-03 23:08:03 +0700

    Apus komen

--- app/src/main/java/com/id/start/domma_v2/fitur/LoginActivity.java
+++ app/src/main/java/com/id/start/domma_v2/fitur/LoginActivity.java
@@ -123,8 +123,6 @@ public class LoginActivity extends AppCompatActivity{
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
-                            //up dateUI(null);
                         }
 
                         // ...
